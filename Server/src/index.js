3eeae4debf9496c1318138c0572cb8247a5d0dfd
chaithanyasp <113092741+chaithanyasp@users.noreply.github.com>
2022-11-29 17:06:11 +0530

    loginschema created

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
-const loginRoutes = require("./routes/crud");
+const formRoutes = require("./routes/crud");
+const loginRoutes = require("./routes/loginRoutes");
 require("./connectionAndSchema/config");
  
 
-app.use("/api/property",loginRoutes)
+app.use("/api/property",formRoutes)
+app.use("/api/user",loginRoutes)
   app.listen(port, () => console.log(`App listening on port ${port}!`));
 
   module.exports = app;
