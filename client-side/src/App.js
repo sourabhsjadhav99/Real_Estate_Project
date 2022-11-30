a1938a73086f240635c7a1df76a91836e0d6e37b
Mayur Ramteke <mayurramteke7575@gmail.com>
2022-12-01 00:59:20 +0530

    ADDED ROUTES IN APP.JS FILE

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
+import React, {useState} from "react"; 
+import { BrowserRouter, Routes ,Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const [useInfo, setUseInfo] = useState({
+    userId : '',
+    userName : '',
+    accessToken : ''
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<SignIn/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/display" element={<Forms/>}/>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
