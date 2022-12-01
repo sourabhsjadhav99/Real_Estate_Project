f0466779000705eb2c7f533ad1c2911c4457e6fe
sourabhsjadhav99 <sourabhsjadhav99@gmail.com>
2022-12-01 07:09:13 -0800

    unecessary files removed

--- FrontEnd/Client/src/index.js
+++ FrontEnd/Client/src/index.js
@@ -4,22 +4,15 @@ import "./index.css";
 import App from "./App";
 // import Apps from "./Apps";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//document.getElementById('root')
-// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
