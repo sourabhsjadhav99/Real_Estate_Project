2eb7e2cee2703645f2fa6f4d57fa048a237fc0ae
Sourabh Shivaji Jadhav <103258218+sourabhsjadhav99@users.noreply.github.com>
2022-12-05 04:38:29 -0800

    Delete data.js

--- Server/src/connectionAndSchema/data.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const dataSchema =  mongoose.Schema({
-    userid:{type:String,require:true},
-    email:{type:String,require:true},
-    password:{type:String,require:true}
-})
-const Data = mongoose.model('info',dataSchema);
-module.exports = Data;
